111f3448736d537cf20747dafa043490a6e8f5c6
Nicolás Zelaya <fezelayani@gmail.com>
2024-01-16 21:33:29 -0400

    Specific GET method and POST method implemented

--- src/main/java/bo/usfx/springneuroapi/controller/NeurodiversityController.java
+++ src/main/java/bo/usfx/springneuroapi/controller/NeurodiversityController.java
@@ -3,18 +3,38 @@ package bo.usfx.springneuroapi.controller;
 import bo.usfx.springneuroapi.model.Neurodiversity;
 import bo.usfx.springneuroapi.repository.NeurodivergencyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class NeurodiversityController {
     @Autowired
     private NeurodivergencyRepository neurodivergencyRepository;
-    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/neurodiversities")
+    @GetMapping("/api/v1/neurodiversities")
     public List<Neurodiversity> getAll() {
         return neurodivergencyRepository.findAll();
+    }
+    @GetMapping("/api/v1/neurodiversities/{name}")
+    public ResponseEntity<?> getSingle(@PathVariable("name") String name){
+        Optional<Neurodiversity> neuroOptional = neurodivergencyRepository.findByName(name);
+        if(neuroOptional.isPresent()){
+            return new ResponseEntity<>(neuroOptional.get(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("Neurodivergency not found with name "+name, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/api/v1/neurodiversities")
+    public ResponseEntity<?> createSingle(@RequestBody Neurodiversity neurodiversity){
+        try {
+            neurodivergencyRepository.save(neurodiversity);
+            return new ResponseEntity<Neurodiversity>(neurodiversity,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
