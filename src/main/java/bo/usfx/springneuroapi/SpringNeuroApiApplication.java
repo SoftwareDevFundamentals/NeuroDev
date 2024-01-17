3017631d7e16f304e3c7fe256c4026790ec6203e
Bryan Simón Polo Orellana <102623208+bspoloo@users.noreply.github.com>
2024-01-17 15:10:03 -0400

    added changes

--- src/main/java/bo/usfx/springneuroapi/SpringNeuroApiApplication.java
+++ src/main/java/bo/usfx/springneuroapi/SpringNeuroApiApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class SpringNeuroApiApplication {
+public class SpringNeuroApiApplication {
 
-    private SpringNeuroApiApplication() {
-
-    }
     public static void main(final String[] args) {
         SpringApplication.run(SpringNeuroApiApplication.class, args);
     }
--- src/main/java/bo/usfx/springneuroapi/controller/NeurodiversityController.java
+++ src/main/java/bo/usfx/springneuroapi/controller/NeurodiversityController.java
@@ -5,14 +5,12 @@ import bo.usfx.springneuroapi.repository.NeurodivergencyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
+@RestController
 public final class NeurodiversityController {
     @Autowired
     private NeurodivergencyRepository neurodivergencyRepository;
@@ -25,7 +23,7 @@ public final class NeurodiversityController {
 
 
     //get entity by method getId
-    @GetMapping("/api/v1/neurodiversities/{id}")
+    @GetMapping("/api/v1/neurodiversities/id/{id}")
     public ResponseEntity<?> getById(@PathVariable final String id) {
         var neuro = neurodivergencyRepository.findById(id);
         if (neuro != null) {
@@ -35,7 +33,7 @@ public final class NeurodiversityController {
     }
 
     //get entity by method getName
-    @GetMapping("/api/v1/neurodiversities/{name}")
+    @GetMapping("/api/v1/neurodiversities/name/{name}")
     public ResponseEntity<?> getByName(@PathVariable final String name) {
         var neuro = neurodivergencyRepository.findByName(name);
         if (neuro != null) {
@@ -44,7 +42,7 @@ public final class NeurodiversityController {
         return null;
     }
 
-    @PostMapping("/api/v1/neurodiversity/")
+    @PostMapping("/api/v1/neurodiversity/create")
     public ResponseEntity<?> createEntity(@RequestBody final Neurodiversity neurodiversity) {
         neurodivergencyRepository.save(neurodiversity);
         return new ResponseEntity<>(neurodiversity, HttpStatus.OK);
