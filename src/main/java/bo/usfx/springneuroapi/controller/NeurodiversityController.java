183af22999b643c92fab53d8decc24ea398f1412
Mauricio Flores Garzofino <mauricioflores123321@gmail.com>
2024-01-17 14:09:01 -0400

    Implement CRUD operations in Neurodiversity Controller

--- src/main/java/bo/usfx/springneuroapi/controller/NeurodiversityController.java
+++ src/main/java/bo/usfx/springneuroapi/controller/NeurodiversityController.java
@@ -5,15 +5,25 @@ import bo.usfx.springneuroapi.repository.NeurodivergencyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public final class NeurodiversityController {
+
+    private static final String NEURODIVERSITY_NOT_FOUND_MESSAGE = "Neurodiversity not found for this id: ";
+
     @Autowired
     private NeurodivergencyRepository neurodivergencyRepository;
     /**
@@ -21,7 +31,6 @@ public final class NeurodiversityController {
      *
      * @return a ResponseEntity containing the list of neurodiversities or a not found status
      */
-    // getAll
     @GetMapping("api/v1/neurodiversities")
     public ResponseEntity<Object> getAll() {
         List<Neurodiversity> neurodiversities = neurodivergencyRepository.findAll();
@@ -30,6 +39,13 @@ public final class NeurodiversityController {
         } else {
             return new ResponseEntity<>("No neurodiversities available", HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PostMapping("api/v1/neurodiversities")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Neurodiversity createNeurodiversity(@RequestBody final Neurodiversity neurodiversity) {
+        neurodiversity.setId(UUID.randomUUID().toString().split("-")[0]);
+        return neurodivergencyRepository.save(neurodiversity);
     }
 
     @GetMapping("/api/v1/neurodiversities/name/{name}")
@@ -46,7 +62,31 @@ public final class NeurodiversityController {
         if (neurodiversityOptional.isPresent()) {
             return new ResponseEntity<>(neurodiversityOptional.get(), HttpStatus.OK);
         } else {
-            return new ResponseEntity<>("Neurodiversity not found for this id: " + id, HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(NEURODIVERSITY_NOT_FOUND_MESSAGE + id, HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PutMapping("api/v1/neurodiversities/id/{id}")
+    public ResponseEntity<Neurodiversity> updateNeurodiversity(@PathVariable(value = "id")
+        final String id, @RequestBody final Neurodiversity neurodiversityDetails) {
+        Neurodiversity neurodiversity = neurodivergencyRepository.findById(id)
+                .orElseThrow(() -> new ResourceAccessException(NEURODIVERSITY_NOT_FOUND_MESSAGE + id));
+
+        neurodiversity.setName(neurodiversityDetails.getName());
+        neurodiversity.setDescription(neurodiversityDetails.getDescription());
+        neurodiversity.setWorldWidePercentage(neurodiversityDetails.getWorldWidePercentage());
+        neurodiversity.setBasicExplanationLink(neurodiversity.getBasicExplanationLink());
+        neurodiversity.setTestLink(neurodiversity.getTestLink());
+
+        final Neurodiversity updatedNeurodiversity = neurodivergencyRepository.save(neurodiversity);
+        return ResponseEntity.ok(updatedNeurodiversity);
+    }
+
+    @DeleteMapping("api/v1/neurodiversities/id/{id}")
+    public ResponseEntity<Object> deleteNeurodiversity(@PathVariable(value = "id") final String id) {
+        Neurodiversity neurodiversity = neurodivergencyRepository.findById(id)
+                .orElseThrow(() -> new ResourceAccessException(NEURODIVERSITY_NOT_FOUND_MESSAGE + id));
+        neurodivergencyRepository.delete(neurodiversity);
+        return ResponseEntity.ok().build();
     }
 }
