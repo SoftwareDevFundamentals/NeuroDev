e524fc29a6cfbb7b6c9804094f6a0681eed188aa
btt <waltervaquera17@gmail.com>
2024-01-17 12:11:37 -0400

    Added getByName and post methods

--- src/main/java/bo/usfx/springneuroapi/controller/NeurodiversityController.java
+++ src/main/java/bo/usfx/springneuroapi/controller/NeurodiversityController.java
@@ -3,18 +3,44 @@ package bo.usfx.springneuroapi.controller;
 import bo.usfx.springneuroapi.model.Neurodiversity;
 import bo.usfx.springneuroapi.repository.NeurodivergencyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-public class NeurodiversityController {
+public final class NeurodiversityController {
     @Autowired
     private NeurodivergencyRepository neurodivergencyRepository;
-    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/neurodiversities")
+
+    @GetMapping("/api/v1/neurodiversities")
     public List<Neurodiversity> getAll() {
         return neurodivergencyRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/neurodiversities/{name}")
+    public ResponseEntity<?> getByName(@PathVariable final String name) {
+        Optional<Neurodiversity> neurodiversity = neurodivergencyRepository.findByName(name);
+        if (neurodiversity.isPresent()) {
+            return new ResponseEntity<>(neurodiversity.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Neurodiversity '" + name + "' was not found.", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/api/v1/neurodiversities")
+    public ResponseEntity<?> post(@RequestBody final Neurodiversity neurodiversity) {
+        try {
+            neurodivergencyRepository.save(neurodiversity);
+            return new ResponseEntity<>(neurodiversity, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/bo/usfx/springneuroapi/repository/NeurodivergencyRepository.java
+++ src/main/java/bo/usfx/springneuroapi/repository/NeurodivergencyRepository.java
@@ -3,5 +3,8 @@ package bo.usfx.springneuroapi.repository;
 import bo.usfx.springneuroapi.model.Neurodiversity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface NeurodivergencyRepository extends MongoRepository<Neurodiversity, String>{
+import java.util.Optional;
+
+public interface NeurodivergencyRepository extends MongoRepository<Neurodiversity, String> {
+    Optional<Neurodiversity> findByName(String name);
 }
