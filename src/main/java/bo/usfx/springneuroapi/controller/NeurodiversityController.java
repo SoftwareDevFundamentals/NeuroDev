dfe53021d60d5e78f55f72ebfcc7952c9def0e63
armandonum <armandonunez404@gmail.com>
2024-01-17 21:58:49 -0400

    delete method

--- src/main/java/bo/usfx/springneuroapi/controller/NeurodiversityController.java
+++ src/main/java/bo/usfx/springneuroapi/controller/NeurodiversityController.java
@@ -6,12 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ReflectionUtils;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.lang.reflect.Field;
@@ -78,6 +73,14 @@ public final class NeurodiversityController {
             }
         });
         return neurodivergencyRepository.save(existing);
+    }
+
+
+
+    @DeleteMapping("/api/v1/neurodiversity/{idNeurodiversity}")
+    public ResponseEntity<?> deleteById(@PathVariable final String idNeurodiversity){
+        neurodivergencyRepository.deleteById(idNeurodiversity);
+        return ResponseEntity.ok(null);
     }
     //      //
 }
