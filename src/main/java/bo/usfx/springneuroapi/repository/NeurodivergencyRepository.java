d387f5290630b5e0b24c81e12a22aef2055bd0d2
armandonum <armandonunez404@gmail.com>
2024-01-18 12:23:59 -0400

    key word

--- src/main/java/bo/usfx/springneuroapi/repository/NeurodivergencyRepository.java
+++ src/main/java/bo/usfx/springneuroapi/repository/NeurodivergencyRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 public interface NeurodivergencyRepository extends MongoRepository<Neurodiversity, String> {
 
     List<Neurodiversity> findByName(String name);
+
     @Query("{'description': {$regex : ?0, $options: 'i'}}")
     List<Neurodiversity> findByDescriptionContaining(String keyword);
-    
+
     List<Neurodiversity> findByNameStartingWithIgnoreCase(String name);
 }
-
